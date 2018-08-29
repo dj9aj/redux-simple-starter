0dfdb8ed40729696e90e3009c33bc6b645d60794
Jack Doyle <jackdo@hotmail.co.uk>
2018-08-29 11:46:33 +0100

    Add video list functionality

--- src/index.js
+++ src/index.js
@@ -2,17 +2,27 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyB6O_mQBco8IPgOuiZ3TkRLRoYC4xUs1JU';
 
-YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
-    console.log(data);
-});
+
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { videos: [] };
+
+        YTSearch({key: API_KEY, term: 'surfboards'}, videos => {
+            this.setState({ videos });
+        });
+    }
+
     render() {
         return (
             <div>
                 <SearchBar />
+                <VideoList videos={this.state.videos} />
             </div>
         );
     }   
